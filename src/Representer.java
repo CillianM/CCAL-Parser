806ad7ef0b319710396afc63502f4e27144c9971
Cillian Mc Neill <cillianmcneill@gmail.com>
2017-12-06 18:39:12 +0000

    Adding Variable & Representer class

---
+++ src/Representer.java
@@ -0,0 +1,186 @@
+public class Representer implements CCALParserVisitor {
+    @Override
+    public Object visit(SimpleNode node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTProgramme node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTMain node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTFunctionList node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTFunction node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTFunctionBody node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTReturnStatement node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTParamList node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTParam node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTDeclarationList node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTConstDeclaration node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTVarDeclaration node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTStatementBlock node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTCondition node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTIf node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTWhile node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTSkip node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTNot node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTFunctionCall node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTMinus node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTArgumentList node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTArg node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTVariable node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTDigit node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTTypeValue node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTEqual node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTNotEqual node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTLessThan node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTLessThanEqual node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTGreaterThan node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTGreaterThanEqual node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTOr node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTAnd node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTBoolean node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTAdd node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTSubtract node, Object data) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTAssignment node, Object data) {
+        return null;
+    }
+}
